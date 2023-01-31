27766d5ab930530e5a7e963a4728450ed6621cbb
Marcelo Dantas <dantasmr@yahoo.com.br>
2023-01-31 07:57:58 -0300

    exemplo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # WebAssembly
+
+
+WebAssembly (Wasm) é um padrão de máquina virtual para a web, projetado para ser executado em navegadores com segurança e alta performance. É uma linguagem de baixo nível e binária que permite a execução de aplicativos complexos e computacionalmente intensivos no navegador, oferecendo uma alternativa ao JavaScript.
+
+WebAssembly foi lançado em 17 de março de 2017.
---
+++ hello.js
@@ -0,0 +1,67 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+    console.log('request starting...');
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    console.log(filePath)    
+    var extname = path.extname(filePath);
+    console.log('extname:' + extname);
+    var contentType = 'text/html';
+    switch (extname) {
+    
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+        case '.wasm':
+            contentType = 'application/wasm';
+            break;
+            
+
+    }
+
+    fs.readFile(filePath, function(error, content) {
+
+
+        console.log('contentType:' + contentType);
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8125);
+console.log('Server running at http://127.0.0.1:8125/');
